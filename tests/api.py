7e2b9db1f315e9124b09b888096119fff58b29a9
Jason Novinger <jnovinger@gmail.com>
2014-01-13 23:42:22 -0600

    XCANTS-1556: adds Ping to Pull and Register Pull Interface integration, auth is still a bit messed up.

---
+++ tests/api.py
@@ -0,0 +1,46 @@
+import unittest
+
+import livefyre
+
+
+class ApiTestCase(unittest.TestCase):
+    def setUp(self):
+        token = 'thisisanawesometoken'
+        self.lf = livefyre.Livefyre(network=network,
+                                    network_secret=network_secret,
+                                    site_id=site_id,
+                                    site_secret=secret,
+                                    system_token=token)
+
+    def test_register_profile_pull_interface(self):
+        resp = self.lf.register_profile_pull_interface(
+            url='http://www.domain.com/profile/?id={id}')
+
+        expected = {
+            'url': 'http://dailydot-0.fyre.co/?pull_profile_url=http%3A%2F%2Fwww.domain.com%2Fprofile%2F%3Fid%3D%7Bid%7D&actor_token=thisisanawesometoken',
+            'body': None,
+        }
+
+        self.assertEqual(resp.url, expected['url'])
+        self.assertEqual(resp.request.body, expected['body'])
+
+        print resp.content
+        print resp.url
+
+        self.assertEqual(resp.status_code, 204)
+
+    def test_ping_to_pull(self):
+        resp = self.lf.ping_to_pull('bob_the_builder')
+
+        expected = {
+            'url': 'http://dailydot-0.fyre.co/api/v3_0/user/bob_the_builder/refresh',
+            'body': 'lftoken=thisisanawesometoken',
+        }
+
+        self.assertEqual(resp.url, expected['url'])
+        self.assertEqual(resp.request.body, expected['body'])
+
+        print resp.content
+        print resp.url
+        self.assertEqual(resp.status_code, 204)
+
